9780e89a3f19daa11717ddcf445ea2747fa09682
helmihidzir <helmihidzir@gmail.com>
2019-05-30 13:08:01 +0800

    Configure AWS settings

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,5 +8,7 @@ if Rails.env.production?
         :region                => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
 end
