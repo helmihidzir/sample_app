50eec4052964b9c0c5e087a3431f1f9db88c5cc8
helmihidzir <helmihidzir@gmail.com>
2019-05-30 14:25:00 +0800

    Format production.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,16 +1,19 @@
-CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
 
-  config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :region                => ENV['S3_REGION']
+    config.fog_credentials = {
+          # Configuration for Amazon S3
+          :provider              => 'AWS',
+          :s3_access_key_id     => ENV['S3_ACCESS_KEY'],
+          :s3_secret_access_key => ENV['S3_SECRET_KEY'],
+          :region                => ENV['S3_REGION'],
+          :host                   => 's3.example.com',             # optional, defaults to nil
+          :endpoint               => 'https://s3.example.com:8080'
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_public     = false
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-
+      config.fog_directory     =  ENV['S3_BUCKET']
+      config.fog_public     = false
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  end
 end
