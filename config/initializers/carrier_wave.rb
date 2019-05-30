4367c30bbb286abf359f9d4e0eeaa7935a3e91fb
helmihidzir <helmihidzir@gmail.com>
2019-05-30 14:29:01 +0800

    Change s3 to aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,8 @@ if Rails.env.production?
     config.fog_credentials = {
           # Configuration for Amazon S3
           :provider              => 'AWS',
-          :s3_access_key_id     => ENV['S3_ACCESS_KEY'],
-          :s3_secret_access_key => ENV['S3_SECRET_KEY'],
+          :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+          :aws_secret_access_key => ENV['S3_SECRET_KEY'],
           :region                => ENV['S3_REGION'],
           :host                   => 's3.example.com',             # optional, defaults to nil
           :endpoint               => 'https://s3.example.com:8080'
