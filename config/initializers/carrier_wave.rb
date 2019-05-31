7e60bc5bc0ac889060426e56dc9e809a754f07af
helmihidzir <helmihidzir@gmail.com>
2019-05-31 15:59:34 +0800

    Add back config line

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,10 +7,13 @@ if Rails.env.production?
           :provider              => 'AWS',
           :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
           :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-          :region                => ENV['S3_REGION']
+          :region                => ENV['S3_REGION'],
+          :host                   => 's3.example.com',
+          :endpoint               => 'https://s3.example.com:8080'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_provider = 'fog/aws'
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
 end
 
